2d06b4e11a58851a21e1ecda3501a11586383eb5
Andrew Morris <voltrevo@gmail.com>
2024-05-07 16:06:40 +1000

    Add circuit.eval stub

--- src/Circuit.ts
+++ src/Circuit.ts
@@ -1,4 +1,8 @@
 export default class Circuit {
+  eval(_input: Record<string, unknown>): Record<string, unknown> {
+    throw new Error('Not implemented');
+  }
+
   static fromCircom(_files: Record<string, string>): Circuit {
     throw new Error('Not implemented');
   }
