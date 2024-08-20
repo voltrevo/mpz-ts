33426ae9a3d4df693b5e66f08350dde0024390bb
Andrew Morris <voltrevo@gmail.com>
2024-08-20 15:34:53 +1000

    Fix target config (fixes rust-analyzer)

--- wasm/src/mpz_circuit_from_bristol.rs
+++ wasm/src/mpz_circuit_from_bristol.rs
@@ -1,11 +1,12 @@
+use bristol_circuit::BristolCircuit;
 use serde_wasm_bindgen::from_value;
 use wasm_bindgen::JsValue;
 
-use crate::{circuit::Circuit, mpz_ts_error::MpzTsError};
+use crate::mpz_ts_error::MpzTsError;
 use mpz_circuits::Circuit as MpzCircuit;
 
 pub fn mpz_circuit_from_bristol(circuit: JsValue) -> Result<MpzCircuit, MpzTsError> {
-    let circuit = from_value::<Circuit>(circuit)?;
+    let circuit = from_value::<BristolCircuit>(circuit)?;
 
     todo!()
 }
--- wasm/src/mpz_ts_error.rs
+++ wasm/src/mpz_ts_error.rs
@@ -2,5 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum MpzTsError {
+    #[error(transparent)]
     SerdeWasmError(#[from] serde_wasm_bindgen::Error),
 }
